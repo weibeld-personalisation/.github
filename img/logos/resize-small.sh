d89696fd81f73cc0fcc43a0d29dfd0efac0285b3
Daniel Weibel <danielmweibel@gmail.com>
2024-04-26 13:21:21 +0200

    Add original logo files and resize script

---
+++ img/logos/resize-small.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+convert bash-original.png -resize x40 bash-small.png
+convert vim-original.png -resize x40 vim-small.png
+convert tmux-original.png -resize x35 tmux-small.png
+convert git-original.png -resize x35 git-small.png
